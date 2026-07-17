c75ef40a29d8d3c3fb216d891bd198ba7eaad556
nordberger <124420729+nordberger@users.noreply.github.com>
2026-07-17 07:29:34 -0600

    Fix syntax error in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ ingredients_list = st.multiselect(
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
     #st.write(ingredients_list)
